7a9eb1b8d8db5094f2e0c6eb86f54639a93ed8d1
=Håkon Mikalsen <=hakmikalsen@gmail.com>
2024-02-12 13:43:33 +0100

    tester et biblotek

--- test1.py
+++ test1.py
@@ -1,4 +1,6 @@
+import pythonBibloteker.kultBiblotek as kb
 print("hei")
 print("her er en endrring og en ny verjson")
 print("ooops endret noe igjen")
 print("Nå la jeg til noe i en branch")
+print(kb.betterInput("str","hei","fhbd"))

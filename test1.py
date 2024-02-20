f6b9153121f04c89c95e949fe2ee208189a2e8cc
=Håkon Mikalsen <=hakmikalsen@gmail.com>
2024-02-20 09:37:20 +0100

    fjernet input og la til en del hei

--- test1.py
+++ test1.py
@@ -3,4 +3,7 @@ print("hei")
 print("her er en endrring og en ny verjson")
 print("ooops endret noe igjen")
 print("Nå la jeg til noe i en branch")
-print(kb.betterInput("str","hei","fhbd"))
+# print(kb.betterInput("str","hei","fhbd"))
+
+for i in range(100):
+    print("hei")

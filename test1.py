cc28b067d06734c50086f00660009ced36f6c8d6
=Håkon Mikalsen <=hakmikalsen@gmail.com>
2024-02-20 10:52:43 +0100

    nå skirver koden også ut hei på deg

--- test1.py
+++ test1.py
@@ -2,5 +2,5 @@ import kultBiblotek.kultBiblotek as kb
 print("hei")
 print("her er en endrring og en ny verjson")
 print("ooops endret noe igjen")
-
+print("hei på deg")
 print(kb.betterInput("str","hei","fhbd"))

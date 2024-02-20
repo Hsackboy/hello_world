706474c0b83b8c774034572a1d9e501c667f9fc1
=Håkon Mikalsen <=hakmikalsen@gmail.com>
2024-02-20 09:42:02 +0100

    grusab

--- test1.py
+++ test1.py
@@ -2,5 +2,5 @@ import kultBiblotek.kultBiblotek as kb
 print("hei")
 print("her er en endrring og en ny verjson")
 print("ooops endret noe igjen")
-print("Nå la jeg til noe i en branch")
+
 print(kb.betterInput("str","hei","fhbd"))
